0d59f1200f7143b0816aef52c435887231de1bac
Abubakar Abdul-Wali <abubakarabdulwali7@gmail.com>
2025-04-20 14:27:46 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,22 +1,22 @@
 // Create a container for the widget
 const container = document.createElement('div');
-container.id = 'chatbot_frontend-container';
+container.id = 'chatbot-widget-container';
 document.body.appendChild(container);
 
 // Load CSS
 const link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'https://wali-dt.github.io/chatbot_frontend/style.css';
+link.href = 'https://wali-dt.github.io/chatbot-widget/style.css';
 document.head.appendChild(link);
 
 // Load HTML
-fetch('https://yourusername.github.io/chatbot_frontend/widget.html')
+fetch('https://wali-dt.github.io/chatbot-widget/widget.html')
   .then(res => res.text())
   .then(html => {
     container.innerHTML = html;
 
     // Load JS
     const script = document.createElement('script');
-    script.src = 'https://yourusername.github.io/chatbot_frontend/script.js';
+    script.src = 'https://wali-dt.github.io/chatbot-widget/script.js';
     document.body.appendChild(script);
   });
