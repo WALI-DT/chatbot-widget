adf5d02694a2416f5087f2710af6a6272fdf10bb
Abubakar Abdul-Wali <abubakarabdulwali7@gmail.com>
2025-04-20 14:26:30 +0100

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,5 +1,5 @@
 (function () {
   const script = document.createElement('script');
-  script.src = 'https://wali-dt.github.io/chatbot_frontend/embed.js'; // this script will inject the widget
+  script.src = 'https://wali-dt.github.io/chatbot-widget/embed.js'; // this script will inject the widget
   document.body.appendChild(script);
 })();
