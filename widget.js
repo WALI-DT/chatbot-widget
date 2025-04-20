16bf2b8e1dcbde92ce474e1c5acde885b6d52c05
Abubakar Abdul-Wali <abubakarabdulwali7@gmail.com>
2025-04-20 14:18:54 +0100

    Create widget.js

---
+++ widget.js
@@ -0,0 +1,5 @@
+(function () {
+  const script = document.createElement('script');
+  script.src = 'https://wali-dt.github.io/chatbot-widget/embed.js'; // this script will inject the widget
+  document.body.appendChild(script);
+})();
